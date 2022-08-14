f41e6685e7bce02bf1d45ae177a5842f0e1cbaa8
Max Mizantrop <96974431+muesq@users.noreply.github.com>
2022-08-14 21:46:36 +0300

    main program
    da

---
+++ cringe.py
@@ -0,0 +1,65 @@
+#ver0.01a
+
+name = input("Введите ваше имя ---> ")
+
+input("Введите ваш пароль ---> ")
+
+print("Добро пожаловать, " + name)
+
+#короч, надо придумать, как выглядел бы мессенджер в консоли, XD
+
+print("\nМеню: ")
+
+print("1) Написать сообщение")
+print("2) Входящие сообщения")
+print("3) Исходяцие сообщения")
+print("4) Спам")
+print("5) Настройки")
+print("6) superdupermenuforfreerobux")
+
+answ = input("---> ")
+
+if answ == ("1"):
+    print("В РАЗРАБОТКЕ")
+
+if answ == ("2"):
+    print("В РАЗРАБОТКЕ")
+
+if answ == ("3"):
+    print("В РАЗРАБОТКЕ")
+
+if answ == ("4"):
+    print("В РАЗРАБОТКЕ")
+
+if answ == ("5"):
+    print("1) Удалить аккаунт и взорвать к хуям хохляндию")
+    input("---> ")
+
+    import time
+
+    for percent in range(101):
+        s = f"[{(percent // 10) * '■'}"
+        s += f"{(10 - (percent // 10)) * '○'}] "
+        s += f"{percent}"
+        print(s, end="\r")
+        time.sleep(0.1)
+
+    print("\nГАТОВА!")
+
+#меня рома нахуй убьёт
+if answ == ("6"):
+    print("how much u want ROBUX")
+    robux = input("---> ")
+
+    import time
+
+    for percent in range(101):
+        s = f"[{(percent // 10) * '■'}"
+        s += f"{(10 - (percent // 10)) * '○'}] "
+        s += f"{percent}"
+        print(s, end="\r")
+        time.sleep(0.1)
+
+    print("\ndone ^_^. now you have " + robux + " robux in yr balance")
+
+#beluga = hecker
