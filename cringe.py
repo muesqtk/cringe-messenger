6a5c2b6eaa1429670076579b818a15182ef55103
Max Mizantrop <96974431+muesqtk@users.noreply.github.com>
2022-08-15 18:01:53 +0300

    Add files via upload

--- cringe.py
+++ cringe.py
@@ -1,65 +1,105 @@
-#ver0.01a
+#ver0.02a
 
-name = input("Введите ваше имя ---> ")
+print("Здравствуйте, у вас уже есть аккаунт или нет? (д/н)")
 
-input("Введите ваш пароль ---> ")
+puss = input("---> ")
 
-print("Добро пожаловать, " + name)
+if puss == ("д"):
+    name = input("Введите ваше имя ---> ")
 
-#короч, надо придумать, как выглядел бы мессенджер в консоли, XD
+    input("Введите ваш пароль ---> ")
 
-print("\nМеню: ")
+    print("Добро пожаловать, " + name)
 
-print("1) Написать сообщение")
-print("2) Входящие сообщения")
-print("3) Исходяцие сообщения")
-print("4) Спам")
-print("5) Настройки")
-print("6) superdupermenuforfreerobux")
+    # короч, надо придумать, как выглядел бы мессенджер в консоли, XD
 
-answ = input("---> ")
+    print("\nМеню: ")
 
-if answ == ("1"):
-    print("В РАЗРАБОТКЕ")
+    print("1) Написать сообщение")
+    print("2) Входящие сообщения")
+    print("3) Исходяцие сообщения")
+    print("4) Спам")
+    print("5) Настройки")
+    print("6) superdupermenuforfreerobux")
 
-if answ == ("2"):
-    print("В РАЗРАБОТКЕ")
+    answ = input("---> ")
 
-if answ == ("3"):
-    print("В РАЗРАБОТКЕ")
+    if answ == ("1"):
+        print("Кому хотите отправить?")
+        vkusno = input("---> ")
 
-if answ == ("4"):
-    print("В РАЗРАБОТКЕ")
+        print("Введите тему сообщения (если нет темы, нажми Enter)")
+        input("---> ")
 
-if answ == ("5"):
-    print("1) Удалить аккаунт и взорвать к хуям хохляндию")
-    input("---> ")
+        print("Введите текст сообщения")
+        input("--->")
 
-    import time
+        print("Вы хотите отправить сообщение " + vkusno + "? (д/н)")
+        kruto = input("--->")
 
-    for percent in range(101):
-        s = f"[{(percent // 10) * '■'}"
-        s += f"{(10 - (percent // 10)) * '○'}] "
-        s += f"{percent}"
-        print(s, end="\r")
-        time.sleep(0.1)
+        if kruto == ("д"):
 
-    print("\nГАТОВА!")
+            import time
 
-#меня рома нахуй убьёт
-if answ == ("6"):
-    print("how much u want ROBUX")
-    robux = input("---> ")
+            for percent in range(101):
+                s = f"[{(percent // 10) * '■'}"
+                s += f"{(10 - (percent // 10)) * '○'}] "
+                s += f"{percent}"
+                print(s, end="\r")
+                time.sleep(0.01)
 
-    import time
+            print("\nГотово!")
 
-    for percent in range(101):
-        s = f"[{(percent // 10) * '■'}"
-        s += f"{(10 - (percent // 10)) * '○'}] "
-        s += f"{percent}"
-        print(s, end="\r")
-        time.sleep(0.1)
+        if kruto == ("н"):
+            print("ну ладно")
 
-    print("\ndone ^_^. now you have " + robux + " robux in yr balance")
+    if answ == ("2"):
+        print("У вас пока нет новый сообщений")
 
-#beluga = hecker
+    if answ == ("3"):
+        print("Вы пока ничего не отправляли")
+
+    if answ == ("4"):
+        print("У вас нет нового спама")
+
+    if answ == ("5"):
+        print("1) Удалить аккаунт и взорвать к хуям хохляндию")
+        print("2) Поменять цвет терминала")
+
+        accord = input("---> ")
+
+        if accord == ("1"):
+            import time
+
+            for percent in range(101):
+                s = f"[{(percent // 10) * '■'}"
+                s += f"{(10 - (percent // 10)) * '○'}] "
+                s += f"{percent}"
+                print(s, end="\r")
+                time.sleep(0.1)
+
+            print("\nГАТОВА!")
+
+        if accord == ("2"):
+            print("зачем тебе его менять? мы же в 90-ых")
+
+    # меня рома нахуй убьёт
+    if answ == ("6"):
+        print("how much u want ROBUX")
+        robux = input("---> ")
+
+        import time
+
+        for percent in range(101):
+            s = f"[{(percent // 10) * '■'}"
+            s += f"{(10 - (percent // 10)) * '○'}] "
+            s += f"{percent}"
+            print(s, end="\r")
+            time.sleep(0.1)
+
+        print("\ndone ^_^. now you have " + robux + " robux in yr balance")
+
+    # beluga = hecker
+
+if puss == ("н"):
+    print("ну тут короче пока регистрации нет)))")
